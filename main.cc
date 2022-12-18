d06a69d2a77d780c3ae2be0c1d40fd3b37bedf7b
Qing Zheng <qzheng@lanl.gov>
2022-12-18 15:30:51 -0700

    add test code

--- main.cc
+++ main.cc
@@ -1,9 +1,30 @@
-#include <iostream>
-
 #include <duckdb.hpp>
 
 int main() {
-    duckdb::DuckDB db(nullptr);
-    duckdb::Connection con(db);
-    return 0;
+  duckdb::DBConfig conf;
+  conf.maximum_threads = 32;
+  duckdb::DuckDB db(nullptr, &conf);
+  duckdb::Connection con(db);
+  con.Query(
+      "CREATE VIEW particles AS SELECT * FROM "
+      "'/Users/qingzheng/CLionProjects/c2/c2-vpic-sample-dataset-parquet/"
+      "particles/*.parquet'");
+  std::unique_ptr<duckdb::QueryResult> r =
+      con.SendQuery("SELECT * FROM particles");
+  int total = 0;
+  int j = 0;
+  for (duckdb::QueryResult* a = r.get(); a; a = a->next.get()) {
+    fprintf(stderr, "==Query result %d:\n", j);
+    std::unique_ptr<duckdb::DataChunk> d = r->FetchRaw();
+    int i = 0;
+    while (d) {
+      fprintf(stderr, "> Chunk: %d-%d (%d)\n", j, i++, d->size());
+      // d->Print();
+      total += d->size();
+      d = r->FetchRaw();
+    }
+    j++;
+  }
+  fprintf(stderr, "total rows: %d\n", total);
+  return 0;
 }
